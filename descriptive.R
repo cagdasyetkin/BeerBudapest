62648f226e205cce3f79a8acca4eb48808f7aee9
cagdasyetkin <cagdasyetkin@yahoo.com>
2017-10-07 20:54:00 +0200

    after adding histograms

--- descriptive.R
+++ descriptive.R
@@ -17,3 +17,17 @@ df_beer$StdevDreher <- round(sd(df_beer$price_dreher), 2)
 
 df_beer$VarCheap <- round(var(df_beer$price_cheapest), 2)
 df_beer$VarDreher <- round(var(df_beer$price_dreher), 2)
+
+#histograms
+library(ggplot2)
+
+ggplot(df_beer, aes(price_cheapest)) + geom_histogram()
+ggplot(df_beer, aes(price_cheapest)) + geom_histogram(binwidth = 5)
+ggplot(df_beer, aes(price_cheapest)) + geom_histogram(binwidth = 15)
+
+ggplot(df_beer, aes(price_dreher)) + geom_histogram()
+ggplot(df_beer, aes(price_dreher)) + geom_histogram(binwidth = 5)
+ggplot(df_beer, aes(price_dreher)) + geom_histogram(binwidth = 15)
+
+
+
